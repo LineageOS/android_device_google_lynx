ce357b7cfd0629aac5591c8681bb816874a02abd
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-04 01:01:42 +0530

    lynx: Set up proprietary files extraction
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I17b78ccf212d8364070c40fe3c7d06d4048e3fe4

--- lineage_lynx.mk
+++ lineage_lynx.mk
@@ -27,3 +27,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="lynx-user 14 UP1A.231005.007 10754064 release-keys"
 
 BUILD_FINGERPRINT := google/lynx/lynx:14/UP1A.231005.007/10754064:user/release-keys
+
+$(call inherit-product, vendor/google/lynx/lynx-vendor.mk)
