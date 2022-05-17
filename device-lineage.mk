14d89c762b2549646350ae8e83a0be6e60c7ce2e
Michael Bestas <mkbestas@gmail.com>
2022-05-18 00:03:07 +0530

    lynx: Add HbmSVManager RRO overlay
    * Based on redfin
    
    Change-Id: I54d885c233151498372e1b510284d6a297ac3e94

--- device-lineage.mk
+++ device-lineage.mk
@@ -11,6 +11,10 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# HBM
+PRODUCT_PACKAGES += \
+    HbmSVManagerOverlayLynx
+
 # Build necessary packages for vendor
 
 # Audio
