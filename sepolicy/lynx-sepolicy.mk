56ebe101f9e8b0fca66062ae79ed738ff81c08be
Michael Bestas <mkbestas@lineageos.org>
2022-11-04 03:28:51 +0200

    lynx: Update sepolicy path
    Change-Id: Ia24ef37000ed9e1cdf994a32447b0250e3f89186

--- sepolicy/lynx-sepolicy.mk
+++ sepolicy/lynx-sepolicy.mk
@@ -1,3 +1,3 @@
 # sepolicy that are shared among devices using whitechapel
-BOARD_SEPOLICY_DIRS += device/google/lynx-sepolicy/vendor
-BOARD_SEPOLICY_DIRS += device/google/lynx-sepolicy/tracking_denials
+BOARD_SEPOLICY_DIRS += device/google/lynx/sepolicy/vendor
+BOARD_SEPOLICY_DIRS += device/google/lynx/sepolicy/tracking_denials
