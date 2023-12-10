9096397971cbf2e8861642577c573a349732ea7a
Michael Bestas <mkbestas@lineageos.org>
2023-12-10 16:53:32 +0200

    Update fingerprint/build description from UQ1A.231205.015
    Change-Id: I82f2f1e05d4f8f717e00281bda80c2ee91c76536

--- lineage_lynx.mk
+++ lineage_lynx.mk
@@ -24,8 +24,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=lynx \
-    PRIVATE_BUILD_DESC="lynx-user 14 UP1A.231105.003 11010452 release-keys"
+    PRIVATE_BUILD_DESC="lynx-user 14 UQ1A.231205.015 11084887 release-keys"
 
-BUILD_FINGERPRINT := google/lynx/lynx:14/UP1A.231105.003/11010452:user/release-keys
+BUILD_FINGERPRINT := google/lynx/lynx:14/UQ1A.231205.015/11084887:user/release-keys
 
 $(call inherit-product, vendor/google/lynx/lynx-vendor.mk)
