62cd62462d235a64cbafcfc69181ea89d01745dc
Michael Bestas <mkbestas@lineageos.org>
2024-07-03 18:18:57 +0300

    Update fingerprint/build description from AP2A.240705.004
    Change-Id: I571f738c492985a03a1c81d592f624229699ab22

--- lineage_lynx.mk
+++ lineage_lynx.mk
@@ -24,8 +24,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=lynx \
-    PRIVATE_BUILD_DESC="lynx-user 14 AP2A.240605.024 11860263 release-keys"
+    PRIVATE_BUILD_DESC="lynx-user 14 AP2A.240705.004 11875680 release-keys"
 
-BUILD_FINGERPRINT := google/lynx/lynx:14/AP2A.240605.024/11860263:user/release-keys
+BUILD_FINGERPRINT := google/lynx/lynx:14/AP2A.240705.004/11875680:user/release-keys
 
 $(call inherit-product, vendor/google/lynx/lynx-vendor.mk)
