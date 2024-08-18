ef8a056e1d15c3478fc6b1a6ef0c02851dbcaa90
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-18 09:52:31 +0300

    lynx: skip dependencies which aren't defined in .bp
    Change-Id: I01a653661856495788c05b720c0577dac44998a4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -53,6 +53,9 @@ function lib_to_package_fixup_vendor_variants() {
         libprotobuf-cpp-lite-21.12)
             echo "libprotobuf-cpp-lite"
             ;;
+        libacryl | \
+            libexynosv4l2 | \
+            libwpa_client) ;;
         *)
             return 1
             ;;
