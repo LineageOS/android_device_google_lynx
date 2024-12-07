dea03a24f487343d783d99dc6d88c53f30cf300d
Michael Bestas <mkbestas@gmail.com>
2024-12-07 16:12:38 +0200

    Revert "Set system property for Bluetooth Auto On feature"
    This reverts commit 760d0ae689dc80937a8638d477f3a0a7158e828c.
    
    Issue: calyxos#2767
    Change-Id: I849f8000839740ef1627e36f84b1b4556271843c

--- bluetooth/qti_default.mk
+++ bluetooth/qti_default.mk
@@ -20,10 +20,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 	bluetooth.profile.ccp.server.enabled?=false \
 	bluetooth.profile.vcp.controller.enabled?=false \
 
-# Bluetotoh Auto On feature
-PRODUCT_PRODUCT_PROPERTIES += \
-	bluetooth.server.automatic_turn_on=true
-
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
 	android.hardware.bluetooth.prebuilt.xml \
