21947b5d21675757835def684b81d401c575800d
Michael Bestas <mkbestas@gmail.com>
2025-06-14 02:02:07 +0300

    Update fingerprint/build description from BP2A.250605.031.A2
    Change-Id: I67068778abe46a0f72e1bdd0ee8aeac14969f58b

--- lineage_lynx.mk
+++ lineage_lynx.mk
@@ -25,9 +25,9 @@ PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="lynx-user 15 BP1A.250505.005.B1 13277630 release-keys" \
-    BuildFingerprint=google/lynx/lynx:15/BP1A.250505.005.B1/13277630:user/release-keys \
+PRODUCT_BUILD_PROP_OVERRIDES := \
+    BuildDesc="lynx-user 16 BP2A.250605.031.A2 13578606 release-keys" \
+    BuildFingerprint=google/lynx/lynx:16/BP2A.250605.031.A2/13578606:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
